ee82219164ba2215f392da7f0a407a3878755177
SRINI <sriniks887@gmail.com>
2023-02-06 08:32:38 +0530

    created store component and add router link

--- src/app/Components/Auth/login/login.component.ts
+++ src/app/Components/Auth/login/login.component.ts
@@ -1,6 +1,7 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
+import { Route, Router } from '@angular/router';
 import { Login } from 'src/app/Models/Login/LoginClass/login';
 import { UserRegister } from 'src/app/Models/Register/Register-Class/user-register';
 import { LoginSpringService } from 'src/app/Services/Auth/Login/LoginSpring/login-spring.service';
@@ -13,13 +14,13 @@ import { LoginSpringService } from 'src/app/Services/Auth/Login/LoginSpring/logi
 export class LoginComponent {
   
   loginUser:Login=new Login();
-  constructor(public loginService:LoginSpringService,public dialogReference:MatDialogRef<LoginComponent>){}
+  constructor(public loginService:LoginSpringService,public dialogReference:MatDialogRef<LoginComponent>,private route:Router){}
 
   login(){
     this.loginService.loginValidation(this.loginUser).subscribe(
       data=>{
-      
         this.onClose()
+        this.route.navigate(['/StoreProducts'])
       },
       (error:HttpErrorResponse)=>{
         console.log(error.error.message)
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './Components/Auth/login/login.component';
 import { RegisterComponent } from './Components/Auth/register/register.component';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { StoreProductsComponent } from './Components/Main/Products/Store/store-products/store-products.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
+  {path:'StoreProducts',component:StoreProductsComponent}
 ];
 
 @NgModule({
